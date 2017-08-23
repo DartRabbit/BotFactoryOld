c14e2f388f9cf2c3bdfdb26ce4366a66bdea5aac
DartRabbit <dartrabbit02@gmail.com>
2017-08-23 14:28:21 +0300

    JPA config & postgres properties

--- src/main/java/botfactory/BotFactory.java
+++ src/main/java/botfactory/BotFactory.java
@@ -1,30 +1,27 @@
 package botfactory;
 
+import botfactory.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories
-@Import(RepositoryRestMvcAutoConfiguration.class)
-@ComponentScan
 @EnableAutoConfiguration
-public class AppConfig extends SpringBootServletInitializer {
+public class BotFactory extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AppConfig.class);
+        return application.sources(BotFactory.class);
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(AppConfig.class);
+        SpringApplication.run(new Class<?>[] {BotFactory.class, JpaConfig.class}, args);
     }
 }
